679cbb2d54b0500ad2156c44985647eea14db48f
eleenkmail <eleenkmail51@gmail.com>
2022-12-28 00:32:12 +0200

    Gradient Boost Trees

--- service.py
+++ service.py
@@ -5,9 +5,13 @@ import numpy as np
 import uvicorn
 import pickle
 
-# import pipline
-pickle_pip =  open('/Users/aleenkmail/Desktop/CarPricePrediction2/pip_model.pkl', 'rb') 
-pipline = pickle.load(pickle_pip)
+# import models
+
+pickle_knn_pip =  open('/Users/aleenkmail/Desktop/CarPricePrediction2/models/knn_pipline.pkl', 'rb')
+pickle_gbt_pip =  open('/Users/aleenkmail/Desktop/CarPricePrediction2/models/gbt_pipline.pkl', 'rb')
+knn_model = pickle.load(pickle_knn_pip)
+gbt_model = pickle.load(pickle_gbt_pip)
+
 
 
 # Define application
@@ -69,20 +73,18 @@ def _get_predict(feature: Car):
     data =  np.array([color, fueil, license, name, gear, glass, weel_drive,\
          prevـuse, prev_owners, passengers, engine, meter, year]).reshape(1, -1)
     
-    # features = np.array([[color, glass ,name, fueil, prevـuse, license, gear, weel_drive, year, engine, passengers, meter, prev_owners]]).reshape(1, -1)
-    # # data= pd.DataFrame(c, columns=['year', 'engine', 'meter','prev_owners', 'passengers', 'name',\
-    # #      'color', 'fueil', 'prevـuse', 'license', 'gear', 'glass', 'weel_drive'])
-    # # features = np.array(list(vars(feature).values())[0:-1])
 
 
     print(data)
 
     # predict the price
-    predicted_price = pipline.predict(data)
-
+    knn_predicted_price = knn_model.predict(data)
+    gbt_predicted_price = gbt_model.predict(data)
  
     
-    return {"predicted" : int(predicted_price[0]) }
+    return {"price predicted with knn :" : int(knn_predicted_price[0]),
+            "price predicted with gbt :" : int(gbt_predicted_price[0]),
+            }
 
 if __name__ == "__main__":
     uvicorn.run("service:app", host= '0.0.0.0', port= 8000, reload= True)
